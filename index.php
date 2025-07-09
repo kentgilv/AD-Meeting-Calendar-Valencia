2f57ef6626ce71e9a698abc4a4cb06a2d9ed556e
kentgilv <kent.v.valencia@gmail.com>
2025-07-09 13:22:43 +0800

    feat(landing page | index.php): added postgresql and mongodb checker

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once BASE_PATH . '/bootstrap.php';
+require_once BASE_PATH . '/vendor/autoload.php';
+require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
+require_once HANDLERS_PATH . '/postgreChecker.handler.php';
+?>
