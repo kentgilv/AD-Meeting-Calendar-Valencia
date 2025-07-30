a7f1002fa2bba7ca1c845da6fef7fcf30d9682bd
kentgilv <kent.v.valencia@gmail.com>
2025-07-30 15:58:49 +0800

    feat(scripts):added scripts and

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
-require_once BASE_PATH . '/bootstrap.php';
-require_once BASE_PATH . '/vendor/autoload.php';
-require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
-require_once HANDLERS_PATH . '/postgreChecker.handler.php';
+
+$pageTitle = "Home | Meeting Calendar";
+$cssFile = './assets/css/style.css';
+require_once COMPONENTS_PATH . '/templates/head.component.php';
+
+?>
+<main>
+  <!-- Hero Section -->
+  <section id="heroSection" class="hero-section">
+    <div class="hero-content">
+      <!-- Hero Image -->
+      <div class="hero-image">
+        <h1>Meeting Calendar</h1>
+      </div>
+      <!-- Hero Text -->
+      <div class="hero-text">
+        <h2>
+          Set schedules and collaborate.
+        </h2>
+        <a id="getStartedBtn" href="" title="Click Here to Get Started">Get Started</a>
+      </div>
+    </div>
+  </section>
+  <!-- Explore Section -->
+
+</main>
+
+<?php
+$jsFile = './assets/js/script.js';
+require_once COMPONENTS_PATH . '/templates/footer.component.php';
+require_once COMPONENTS_PATH . '/templates/foot.component.php';
 ?>
